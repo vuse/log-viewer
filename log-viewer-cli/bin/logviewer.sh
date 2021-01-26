b2a4f03d7c5a13530daa41c88761e3043bfe6d4c
vuse <vuse@users.noreply.github.com>
2021-01-25 20:32:13 -0500

    Update logviewer.sh
    Startup script for systems without bash installed.

--- log-viewer-cli/bin/logviewer.sh
+++ log-viewer-cli/bin/logviewer.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#!/bin/sh
+SCRIPT_DIR="$( cd $(dirname $0) && pwd )"
 java -ea \
    -Dlog-viewer.config-file=$SCRIPT_DIR/config.conf \
    -jar $SCRIPT_DIR/lib/log-viewer-cli-${project.version}.jar startup
